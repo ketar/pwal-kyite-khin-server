8c5c88a526000f1cce1a23132dae0264865c0b67
winhtaikaung <winhtaikaung28@hotmail.com>
2015-06-06 15:25:34 +0630

    Event Controller edit Deleted

--- api/controllers/v_0_1/EventController.js
+++ api/controllers/v_0_1/EventController.js
@@ -25,16 +25,45 @@ module.exports = {
 	},
 	update:function(req,res,next){
 
-		res.json(req.params.id);
+		//res.json(req.params.id);
+
+		var obj_event={
+			
+			name:(req.body.name!==undefined)?req.body.name:res.json({"error":"Invalid Field"}),
+			type:(req.body.type!==undefined)?req.body.type:res.json({"error":"Invalid Field"}),
+			due_date:(req.body.duedate!==undefined)?req.body.duedate:res.json({"error":"Invalid Field"}),
+			speaker_name:(req.body.speakername!==undefined)?req.body.speakername:res.json({"error":"Invalid Field"}),
+			description:(req.body.desc!==undefined)?req.body.desc:res.json({"error":"Invalid Field"}),
+			is_complete:(req.body.iscomplete!==undefined)?req.body.iscomplete:res.json({"error":"Invalid Field"}),
+			is_ondue:(req.body.due!==undefined)?req.body.due:res.json({"error":"Invalid Field"}),
+			place_id:(req.body.placeid!==undefined)?req.body.placeid:res.json({"error":"Invalid Field"})
+		};
+
+		sails.models.event.update({_id:req.params.id},obj_event,function(err,Object){
+
+			if(!err){res.json({"data":Object})}else{
+				res.json({"error":err});
+			}
+		});
 	},
 	destroy:function(req,res,next){
-		
+		sails.models.event.destroy({_id:req.params.id},function(err,Object){
+			if(err){
+				res.json({"data":"Successfully Deleted"})
+			}else{
+				res.json({"error":err});
+			}	
+		});
 	},
 	find:function(req,res,next){
 		if(req.params.id){
 
 			sails.models.event.findOne(req.params.id,function(err,obj){
-				res.json({"data":obj});
+				if(!obj){
+					res.json({"data":obj},200);
+				}else{
+					res.json({"data":"No Data Found"},200);
+				}
 			});
 
 			//calling 
